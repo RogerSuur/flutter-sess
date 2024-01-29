e23d20c01fdf95c5e87e6879375b4141f122aeb5
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 16:41:59 +0200

    if else2

--- max-num.dart
+++ max-num.dart
@@ -1,12 +1,12 @@
 // Your task is to implement a function maxNum, which should accept three integer parameters and return the maximum of the three.
 
 void main() {
-  print(maxNum(1, 4, 8));
+  print(maxNum(86, -13, 14));
 }
 
 int maxNum(int first, int second, int third) {
   if (first > second && first > third) {
-    return third;
+    return first;
   } else if (second > first && second > third) {
     return second;
   } else if (third > first && third > second) {
