3c6335dd3662f0315f844ff1e58a9a50d26ee0a8
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 16:23:30 +0200

    named optional2

--- named-optional-sum.dart
+++ named-optional-sum.dart
@@ -3,11 +3,23 @@
 // Absent parameters should be considered as 0.
 
 void main() {
-  var result = namedOptionalSum(first: 75, second: -28, third: -74);
+  var result = namedOptionalSum(first: 31, second: 1);
   print(result);
 }
 
-int namedOptionalSum({int? first, int? second, int? third}) {
-  var result = first! + second! + third!;
-  return result;
+// int namedOptionalSum({int? first, int? second, int? third}) {
+//   if (first != null && second != null && third != null) {
+//     return first + second + third;
+//   } else if (first != null && second != null) {
+//     return first + second;
+//   } else if (second != null && third != null) {
+//     return second + third;
+//   } else if (first != null && third != null) {
+//     return first + third;
+//   }
+//   return 0;
+// }
+
+int namedOptionalSum({int first = 0, int second = 0, int third = 0}) {
+  return first + second + third;
 }
