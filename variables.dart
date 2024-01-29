9b1f1879942881e37b39bbc2442d79921965fd2f
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 14:20:37 +0200

    variables

---
+++ variables.dart
@@ -0,0 +1,17 @@
+// Instructions
+
+// Declare and initialize following variables:
+
+//     obj of type Object containing any value;
+//     planet of type String containing planet's name you live on;
+//     year of type int containing current year;
+//     lucky of type bool containing true of false (you decide);
+//     pi which is a constant of type double containing the value of pi with 2 decimal points;
+
+void main() {
+  int year = 2024;
+  bool lucky = true;
+  double pi = 3.14;
+  String planet = "Earth";
+  Object obj = 2;
+}
