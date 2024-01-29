fa6bc44e029323e9b8c015017a680ebb2afdd18a
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 18:46:58 +0200

    class

--- person.dart
+++ person.dart
@@ -16,17 +16,10 @@
 //     height: required
 //     surname: optional
 
-class Person {
-  String name;
-  String cityOfOrigin;
-  int age;
-  String? surname;
-  int height;
-
-  Person(
-      {required this.name,
-      required this.cityOfOrigin,
-      required this.age,
-      required this.height,
-      this.surname});
-}
+Person person = Person(
+  name: "Alice",
+  cityOfOrigin: "Wonderland",
+  age: 28,
+  height: 170,
+  surname: "Liddell", // Optional, can be omitted
+);
