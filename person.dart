39622ae93c5e94a9d284f07375a7a058d9229bdd
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 16:57:45 +0200

    class

---
+++ person.dart
@@ -0,0 +1,24 @@
+// Create a class named Person.
+
+// Its attributes:
+
+//     name: string
+//     surname: string
+//     cityOfOrigin: string
+//     age: int
+//     height: int
+
+// Constructor:
+
+//     name: required
+//     cityOfOrigin: required
+//     age: required
+//     height: required
+//     surname: optional
+
+class Person {
+  String name = 'Roger';
+  String cityOfOrigin = 'Tallinn';
+  int age = 32;
+  int height = 178;
+}
