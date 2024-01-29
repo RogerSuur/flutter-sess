a81d6f5b02a75557c567e193dc8f978bd7342632
Jollyroger <roger.suuressaar@gmail.com>
2024-01-29 14:56:17 +0200

    list

---
+++ data-structures.dart
@@ -0,0 +1,13 @@
+// Declare and initialize following variables:
+
+//     listNum of type List<int>, containing a list of integers. There must be at least 5 elements in the list.
+//     listObj of type List<Object>, containing a list of objects. There must be at least 4 elements in the list.
+//     listStr of type List<String>, containing a list of strings. There should be at least 3 elements in the list.
+//     listList of type List<List<Object>>, containing a list of lists containing listNum, listObj, listStr.
+//     setStr of type Set<String>, containing at least 3 strings.
+//     mapStr of type Map<String, int> containing at least 3 pairs.
+
+List<int> listNum = [1, 2, 3, 4, 5, 6];
+List<String> listStr = ['1', '2', '3'];
+List<Object> listObj = [1, 2, 3, 4];
+List<List> listList = [listNum, listObj, listStr];
